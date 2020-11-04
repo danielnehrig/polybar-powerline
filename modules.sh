d2a3ef8a8a23a980d92557b2e88419b8f05cd622
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-11-04 18:56:23 +0100

    fix for desktop 1920

--- modules.sh
+++ modules.sh
@@ -6,6 +6,19 @@
 # Distributed under terms of the MIT license.
 #
 
+# bar height default 25px needs to be scaled up on higher DPI screens
+export POLY_BAR_HEIGHT=25
+
+text_size=14
+icon_size_2=20
+icon_size_1=14
+line_height=3
+
+# fonts
+export POLY_FONT_0="Material Design Icons:pixelsize=$icon_size_1:style:Solid;$line_height"
+export POLY_FONT_1="SauceCodePro Nerd Font Mono:pixelsize=$text_size:style:regular;$line_height"
+export POLY_FONT_2="SauceCodePro Nerd Font Mono:pixelsize=$icon_size_2:style:regular;6"
+
 # customize here
 export MODULES_CENTER="spacer-slanted-left-grayLight date spacer-slanted-right-grayLight"
 export MODULES_RIGHT="cpu spacer-thin-left-whiteLight song spacer-thin-left-whiteLight pulseaudio spacer-thin-left-whiteLight wireless-network spacer-thicc-left-grayLight vm xkeyboard battery power-menu"
