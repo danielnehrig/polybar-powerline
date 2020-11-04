30d2d0ebf4a2912357e62ad4e2f4b93a3e11d416
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2020-11-04 04:49:34 +0100

    installer fix

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 ## Install
 ## WARNING WILL DELETE ALL CONTENTS IN POLYBAR FOLDER
 
+BASEDIR=$(dirname "$0")
 rm -rf ~/.config/polybar
-ln -s ~/polybar-powerline ~/.config/polybar
+ln -s $(pwd) ~/.config/polybar
