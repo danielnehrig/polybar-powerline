71df59589d86c411d26b23d0bb2e08c0be478068
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-04 14:14:03 +0200

    add modules_custom

--- modules.sh
+++ modules.sh
@@ -16,15 +16,11 @@ icon_size_2=20
 icon_size_1=14
 line_height=3
 
-# changeable settings
-export POLY_NETWORK_WIRLESS_INT="wlp3s0"
-
 # fonts
 export POLY_FONT_0="Material Design Icons:pixelsize=$icon_size_1:style:Solid;$line_height"
 export POLY_FONT_1="Hack Nerd Font Mono:pixelsize=$text_size:style:regular;$line_height"
 export POLY_FONT_2="Hack Nerd Font Mono:pixelsize=$icon_size_2:style:regular;6"
 
-# customize here
 export MODULES_CENTER="spacer-slanted-left-bg_accent_1 date spacer-slanted-right-bg_accent_1"
 export MODULES_RIGHT="cpu spacer-thin-left-whiteLight vpn spacer-thin-left-whiteLight pulseaudio spacer-thicc-left-bg_accent_1 vm xkeyboard battery power-menu"
 export MODULES_LEFT="updates spacer-thicc-right-bg_accent_2 i3 spacer-thin-right-whiteDark xwindow spacer-thicc-right-bg_accent_1"
---
+++ modules_custom.sh
@@ -0,0 +1,7 @@
+# changeable settings
+export POLY_NETWORK_WIRLESS_INT="wlp3s0"
+
+# customize modules here
+export MODULES_CENTER="spacer-slanted-left-bg_accent_1 date spacer-slanted-right-bg_accent_1"
+export MODULES_RIGHT="cpu spacer-thin-left-whiteLight vpn spacer-thin-left-whiteLight pulseaudio spacer-thicc-left-bg_accent_1 vm xkeyboard battery power-menu"
+export MODULES_LEFT="updates spacer-thicc-right-bg_accent_2 i3 spacer-thin-right-whiteDark xwindow spacer-thicc-right-bg_accent_1"
