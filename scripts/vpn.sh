37c39e9febad5b66f175cbfc6fde18bce493d53f
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-07-04 22:30:02 +0200

    vpn size fix

--- scripts/vpn.sh
+++ scripts/vpn.sh
@@ -7,13 +7,13 @@
 #
 amount=$(ps aux | grep openvpn | wc -l)
 if [[ $amount == '1' ]]; then
-  echo "%{F$red}轢%{F-}"
+  echo "%{F$red}%{T3}轢%{T-}%{F-}"
 fi
 
 if [[ $amount == '2' ]]; then
-  echo "%{F$green}歷%{F-}"
+  echo "%{F$green}%{T3}歷%{T-}%{F-}"
 fi
 
 if [[ $amount == '3' ]]; then
-  echo "%{F$green}歷%{F-}"
+  echo "%{F$green}%{T3}歷%{T-}%{F-}"
 fi
