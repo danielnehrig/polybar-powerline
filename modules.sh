f62a251ecfd496b7045738cd55475bd7b3d378e9
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2021-11-09 22:42:04 +0100

    tokyo night fix
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- modules.sh
+++ modules.sh
@@ -12,10 +12,12 @@
 export POLY_BAR_HEIGHT=25
 
 text_size=14
+line_height=3
 
 # fonts
 export POLY_FONT_ICON="Material Design Icons:pixelsize=$text_size:style:Solid;$line_height"
 export POLY_FONT_TEXT_AND_ICON="FiraCode Nerd Font:pixelsize=$text_size:style:Regular;$line_height"
+export POLY_FONT_TEXT_AND_ICON_MONO="FiraCode Nerd Font Mono:pixelsize=16:style:Regular;$line_height"
 
 export POLY_MODULES_CENTER="spacer-slanted-left-bg_accent_1 date spacer-slanted-right-bg_accent_1"
 export POLY_MODULES_RIGHT="cpu spacer-thin-left-whiteLight vpn spacer-thin-left-whiteLight pulseaudio spacer-thicc-left-bg_accent_1 vm xkeyboard battery power-menu"
