329cc2a5fd17b60fce3892db93223cc021115981
Daniel Nehrig <daniel.nehrig@dnehrig.com>
2022-01-03 15:00:51 +0100

    better vm script
    Signed-off-by: Daniel Nehrig <daniel.nehrig@dnehrig.com>

--- scripts/vmStart.sh
+++ scripts/vmStart.sh
@@ -6,13 +6,15 @@
 # Distributed under terms of the MIT license.
 #
 amount=$(virsh -c qemu:///system list --all | grep running | wc -l)
+started=$(virsh -c qemu:///system list --all | sed '/running/!d' | sed -e 's/.    //' | sed 's/ \s.*$//' | sed 's/ //g')
 
 if [[ $amount == '0' ]]; then
-  virsh -c qemu:///system start win10-2
-  notify-send "WIN Started"
+	start=$(virsh -c qemu:///system list --all | sed '/-    /!d' | sed -e 's/-    //g' | sed -e 's/ \s.*$//' | sed -e 's/ //g' | rofi -dmenu)
+	if [[ -n $start ]]; then
+		virsh -c qemu:///system start $start
+	fi
 else
-  virsh -c qemu:///system shutdown win10-2
-  virsh -c qemu:///system shutdown archlinux
-  notify-send "WIN Stopped"
+	while IFS= read -r line; do
+		virsh -c qemu:///system shutdown $line
+	done <<<"$started"
 fi
-
